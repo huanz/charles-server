1f697b2a0763efa1fe87a242a83405253f048851
yuhuanzhu <yuhuanzhu@baidu.com>
2016-04-29 19:04:09 +0800

    add travis

--- app.js
+++ app.js
@@ -12,13 +12,14 @@ import logger from 'koa-logger';
 // import debug from 'debug';
 import AV from 'leanengine';
 
+import forceHttps from './middleware/https';
 import cros from './middleware/cros';
 import index from './routes/index';
 import api from './routes/api';
 
-const APP_ID = process.env.LC_APP_ID || 'z4jzXcAKbNIa0xxluFtIuz80-gzGzoHsz';
-const APP_KEY = process.env.LC_APP_KEY || 'nAxHXqjX9hHPRrlnNxoYBMYk';
-const MASTER_KEY = process.env.LC_APP_MASTER_KEY || '4M07vmyAi1RHLjMmo7oXJYDN';
+const APP_ID = process.env.LEANCLOUD_APP_ID || 'z4jzXcAKbNIa0xxluFtIuz80-gzGzoHsz';
+const APP_KEY = process.env.LEANCLOUD_APP_KEY || 'nAxHXqjX9hHPRrlnNxoYBMYk';
+const MASTER_KEY = process.env.LEANCLOUD_APP_MASTER_KEY || '4M07vmyAi1RHLjMmo7oXJYDN';
 
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
@@ -27,6 +28,9 @@ AV.Cloud.useMasterKey();
 const app = new Koa();
 const router = new Router();
 // const D = debug('charles:server');
+
+// force https
+app.use(forceHttps(AV));
 
 // middlewares
 app.use(convert(bodyparser()));
@@ -56,9 +60,9 @@ app.use(router.routes());
 // 错误处理
 onerror(app);
 
-// 端口一定要从环境变量 `LC_APP_PORT` 中获取。
+// 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
-const PORT = parseInt(process.env.LC_APP_PORT || 3000);
+const PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 'use strict';
-require('babel-core/register');
+require('babel-core/register')({
+    presets: ['es2015-node5', 'stage-3']
+});
 require('./app');
